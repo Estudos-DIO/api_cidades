ed22bdef8bf1258d69391463b10b1f5512ee9599
Mauro Salomão <oruamsalomao@gmail.com>
2021-09-19 23:54:52 -0300

    Correção na biblioteca referente ao 'jsonb'.

--- src/main/java/modelo/Estado.java
+++ src/main/java/modelo/Estado.java
@@ -1,6 +1,9 @@
 package modelo;
 
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
 
 import javax.persistence.Basic;
 import javax.persistence.Column;
@@ -14,6 +17,7 @@ import java.util.List;
 
 @Entity(name = "Estado")
 @Table(name="ESTADO")
+@TypeDefs({@TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)})
 public class Estado {
 
     @Id
