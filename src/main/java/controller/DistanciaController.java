187f3835a315a28fc8f63203090293e191b41667
Mauro Salomão <oruamsalomao@gmail.com>
2021-09-22 21:51:21 -0300

    Alteração no retorno do controller.

--- src/main/java/controller/DistanciaController.java
+++ src/main/java/controller/DistanciaController.java
@@ -4,6 +4,7 @@ import modelo.RaioTerra;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,25 +21,25 @@ public class DistanciaController {
     Logger log = LoggerFactory.getLogger( DistanciaController.class );
 
     @GetMapping("/porPontos")
-    public Double porPontos( @RequestParam(name = "de") final Long cidade1,
-                             @RequestParam(name = "para") final Long cidade2 ) {
+    public ResponseEntity porPontos(@RequestParam(name = "de") final Long cidade1,
+                                    @RequestParam(name = "para") final Long cidade2 ) {
         log.info( "-> porPontos: " );
-        return servicoDistancia.distanciaDosPontosEmMilhas( cidade1, cidade2 );
+        return ResponseEntity.ok().body( servicoDistancia.distanciaDosPontosEmMilhas( cidade1, cidade2 ) );
     }
 
     @GetMapping("/porCubo")
-    public Double porCubo( @RequestParam(name = "de") final Long cidade1,
+    public ResponseEntity porCubo( @RequestParam(name = "de") final Long cidade1,
                            @RequestParam(name = "para") final Long cidade2 ) {
         log.info( "-> porCubo: " );
-        return servicoDistancia.distanciaDoCuboEmMetros( cidade1, cidade2 );
+        return ResponseEntity.ok().body( servicoDistancia.distanciaDoCuboEmMetros( cidade1, cidade2 ) );
     }
 
     @GetMapping("/porMatematica")
-    public Double porMatematica( @RequestParam(name = "de") final Long cidade1,
+    public ResponseEntity porMatematica( @RequestParam(name = "de") final Long cidade1,
                                  @RequestParam(name = "para") final Long cidade2,
                                  @RequestParam final RaioTerra unidade ) {
         log.info("-> porMatemática: ");
-        return servicoDistancia.distanciaUtilizandoMatematica( cidade1, cidade2, unidade );
+        return ResponseEntity.ok().body( servicoDistancia.distanciaUtilizandoMatematica( cidade1, cidade2, unidade ) );
     }
 
 }
